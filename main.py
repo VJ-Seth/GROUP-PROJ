8c7d1c46f544fa9a984d162b8d450ce369a19baa
Viraaj Seth <kingbball249@gmail.com>
2021-12-24 13:20:42 -0600

    I got rid of screen variable and screen vector

---
+++ main.py
@@ -0,0 +1,69 @@
+import pygame
+import colors
+import time
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+from player import Player
+
+
+pygame.init()
+
+S_WIDTH = 1350
+S_HEIGHT = 700
+CLOCK = pygame.time.Clock()
+
+screen = pygame.display.set_mode((S_WIDTH, S_HEIGHT))
+
+all_sprites = pygame.sprite.Group()
+player = Player((S_WIDTH, S_HEIGHT), screen, None)
+all_sprites.add(player)
+        
+
+def draw():
+    screen.fill((0, 0, 0))
+
+    all_sprites.draw(screen)
+    pygame.display.update()
+
+def held_key_movement():
+    pressed_keys = pygame.key.get_pressed()
+
+    if pressed_keys[pygame.K_LEFT] or pressed_keys[pygame.K_a]:
+        player.move(0)
+    #elif pressed_keys[pygame.K_UP] or pressed_keys[pygame.K_w]:
+        player.move(1)
+    elif pressed_keys[pygame.K_RIGHT] or pressed_keys[pygame.K_d]:
+        player.move(2)
+    #elif pressed_keys[pygame.K_DOWN] or pressed_keys[pygame.K_s]:
+        player.move(3)
+
+
+def main():
+    pygame.display.set_caption("Fighting Game")
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running = False
+    
+        all_sprites.update()
+
+        held_key_movement()
+        draw()
+
+        CLOCK.tick(60)
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,59 @@
+import pygame
+import random
+
+from colors import WHITE
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, screen_dimensions, screen, starting_pos):
+        super().__init__()
+
+        # Could be replaced with image drawing
+        self.width, self.height = (50, 50)
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(WHITE)
+
+        self.screen = screen
+        self.rect = self.image.get_rect()
+    
+        self.screen_width, self.screen_height = screen_dimensions
+        self.move_offset = 10
+
+        if not starting_pos:
+            self.rect.x, self.rect.y = (0, 0)
+        else:
+            self.rect.x, self.rect.y = starting_pos
+
+
+    def check_boundary(self):
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if (self.rect.x + self.width) > self.screen_width:
+            self.rect.x = (self.screen_width - self.width)
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if (self.rect.y + self.height) > self.screen_height:
+            self.rect.y = (self.screen_height - self.height)
+
+    def update(self):
+        self.check_boundary()
+        
+    def move(self, move_index):
+        """
+            0 - left
+            1 - up
+            2 - right
+            3 - down
+        """
+
+        if move_index == 0:
+            self.rect.x -= self.move_offset
+        # elif move_index == 1:
+        #     self.rect.y -= self.move_offset
+        elif move_index == 2:
+            self.rect.x += self.move_offset
+        # elif move_index == 3:
+        #     self.rect.y += self.move_offset
+        else:
+            return -1
+
